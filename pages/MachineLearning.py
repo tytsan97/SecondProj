3382500011678fad49e94042b2fa6987873c92c7
tytsan97 <148670527+tytsan97@users.noreply.github.com>
2023-12-21 21:45:38 +0630

    Update MachineLearning.py

--- pages/MachineLearning.py
+++ pages/MachineLearning.py
@@ -2,4 +2,9 @@ import pandas as pd
 import streamlit as st
 
 df = pd.read_csv("water_potability.csv")
-st.write(df)
+ph = st.number_input("Enter pH Level:")
+st.write(ph)
+hard = st.number_input("Enter amount of Hardness:")
+st.write(hard)
+solid = st.number_input("Enter amount of Solids:")
+st.write(solid)
