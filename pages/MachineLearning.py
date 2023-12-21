48179fda29a61174505956eddb8c36812e88221a
tytsan97 <148670527+tytsan97@users.noreply.github.com>
2023-12-22 00:24:12 +0630

    Update MachineLearning.py

--- pages/MachineLearning.py
+++ pages/MachineLearning.py
@@ -17,10 +17,11 @@ if rfmodel:
         turbi = st.number_input("Enter Amount of Turbidity:")
         submitted = st.form_submit_button("Submit")
         if submitted:
-          inputdata = {}                  
+          inputdata = {'ph':ph,'Hardness':hard,'Solids':solid,'Chloramines':ch4,'Sulfate':so4,'Conductivity':conduct,
+                      'Organic_carbon':carbon,'Trihalomethanes':trihalo,'Turbidity':turbi}                  
           features = pd.DataFrame(inputdata,index=[0])         
-          x=data.drop("Suggested Job Role",axis=1)
-          y=data["Suggested Job Role"]
+          x=data.drop("Potability",axis=1)
+          y=data["Potability"]
           x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.25, random_state=25)
           filename = 'gnbmodel'
                    
